f952bfa07ce265247aace4ee782532945b54cf74
mahmoudKSanad <sanadmahmoud50@gmail.com>
2026-06-28 16:41:03 +0300

    done the gallery section

---
+++ assets/JS/script.js
@@ -0,0 +1,18 @@
+let wrapper = document.getElementById('wrapper')
+let prevArrow = document.getElementById('prev')
+let nextArrow = document.getElementById('next')
+let imgs = wrapper.getElementsByTagName('img')
+
+let currentIndex = 1 //   transform: translateX(-25%);
+let total = imgs.length -1
+let moveDestence = imgs[0].width
+
+nextArrow.addEventListener('click', function () {
+  currentIndex = (currentIndex + 1) % total
+  wrapper.style.transform = `translateX(-${moveDestence * currentIndex}px)`
+})
+
+prevArrow.addEventListener('click', function () {
+  currentIndex = (currentIndex - 1 + total) % total
+  wrapper.style.transform = `translateX(-${moveDestence * currentIndex}px)`
+})
