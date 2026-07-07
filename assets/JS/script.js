ac63af717d1bd4befa0e736d4bfac160e14c4ee1
mahmoudKSanad <sanadmahmoud50@gmail.com>
2026-07-07 13:02:24 +0300

    fix responsivness issues

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,19 +6,22 @@ const menu = document.getElementById('menu-icon')
 const navLinks = document.querySelector('nav .links')
 const book = document.querySelectorAll('.book')
 const explore = document.getElementById('explore')
+const ctaBtn = document.getElementById("cta-button")
 
+const bookBtns = [...book, ctaBtn]
 let currentIndex = 1 //   transform: translateX(-25%);
 let total = imgs.length
-let moveDestence = imgs[0].width + 16
+const gap = 16;
+let moveDistance = imgs[0].width + gap
 
 nextArrow.addEventListener('click', function () {
   currentIndex = (currentIndex + 1) % total
-  wrapper.style.transform = `translateX(-${moveDestence * currentIndex}px)`
+  wrapper.style.transform = `translateX(-${moveDistance * currentIndex}px)`
 })
 
 prevArrow.addEventListener('click', function () {
   currentIndex = (currentIndex - 1 + total) % total
-  wrapper.style.transform = `translateX(-${moveDestence * currentIndex}px)`
+  wrapper.style.transform = `translateX(-${moveDistance * currentIndex}px)`
 })
 
 menu.addEventListener('click', () => {
@@ -26,7 +29,7 @@ menu.addEventListener('click', () => {
   navLinks.classList.toggle('opend')
 })
 
-book.forEach((btn) => {
+bookBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     window.open(
       'https://www.facebook.com/gameinnplaystation',
